5679334d415b84e7576bc77ed035f42cb8179125
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-08-26 16:49:43 -0600

    Se agrego un switch y se modificaron las funciones

--- src/main.js
+++ src/main.js
@@ -4,50 +4,69 @@ export function addTwoNumbers(valor1, valor2){
   return console.log(result);
 }
 
-function suma(n1,n2){
-  return n1+n2;
-}
+const suma = (n1,n2)=>{return n1+n2;}
 
-function resta(n1,n2){
-  return n1-n2;
-}
+const resta = (n1,n2)=>{return n1-n2;}
 
-let resultadoResta = resta(1,2);
-console.log(resultadoResta);
+const division = (n1,n2)=>{return n1/n2;}
 
-function division(n1,n2){
-  return n1/n2;
-}
+const suma3Numeros = (n1,n2,n3)=>{return n1+n2+n3;}
 
-let resultadoDivision = division(10,2);
-console.log(resultadoDivision);
 
-function suma3Numeros(n1,n2,n3){
-  return n1+n2+n3;
-}
-
-let resultadoSum3Numeros = suma3Numeros(1,2,3);
-console.log(resultadoSum3Numeros);
-
-function operaciones(n1,n2,n3,operacion){
+const operaciones = (n1,n2,n3,operacion) =>{
   
   let result=0;
-
-  if(operacion[0] == '+'){
-    result = n1 + n2;
-  }else if(operacion[0] == '-'){
-    result = n1 - n2;
-  }
   
-  if(operacion[1] == '+'){
-    result += n3;  
-  }else if(operacion[1] == '-'){
-    result -= n3;
-  }
-
+  if(operacion[0] == '+'){result = n1 + n2;}
+  else if(operacion[0] == '-'){result = n1 - n2;}
+  
+  if(operacion[1] == '+'){result += n3;  }
+  else if(operacion[1] == '-'){result -= n3;}
+  
   return result;
 }
 
 
-let resultadoOperaciones = operaciones(1,2,3,['+','-']);
-console.log(resultadoOperaciones);
+const option = +prompt("Selecciona una opcion:\n 1. Sumar\n 2. Restar\n 3. Dividir\n 4. Sumar 3 Numeros\n 5. Operar");
+
+// console.log(suma(1,2));
+// console.log(resta(1,2));
+// console.log(division(10,2));
+// console.log(suma3Numeros(1,2,3));
+// console.log(operaciones(1,2,3,['+','-']));
+
+switch (option) {
+    case 1:
+        const num1 = +prompt("Ingresa un umero");
+        const num2 = +prompt("Ingresa otro umero");
+        alert(suma(num1,num2));
+        break;
+    case 2:
+        const num3 = +prompt("Ingresa un numero:");
+        const num4 = +prompt("Ingresa otro numero:");
+        alert(resta(num3,num4));
+        break;
+    case 3: 
+        const num5 = +prompt("Ingresa un numero:");
+        const num6 = +prompt("Ingresa otro numero:");
+        alert(division(num5,num6));
+        break;
+    case 4:
+        const num7 = +prompt("Ingresa un numero:");
+        const num8 = +prompt("Ingresa otro numero:");
+        const num9 = +prompt("Ingresa otro numero:"); 
+        alert(suma3Numeros(num7,num8,num9));
+        break;
+    case 5:
+        const num10 = +prompt("Ingresa un numero:");
+        const num11 = +prompt("Ingresa otro numero:");
+        const num12 = +prompt("Ingresa otro numero:");
+        const operador1 = prompt("Escribe uno de los signos: (+) o (-)");
+        const operador2 = prompt("Escribe otro de los signos: (+) o (-)");
+
+        alert(operaciones(num10,num11,num12,[operador1,operador2]));
+
+    default:
+        alert("Opcion no valida");
+}
+
